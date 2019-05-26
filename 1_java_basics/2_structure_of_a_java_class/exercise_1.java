2949e6683673199d36d847156dde6586d52bf76e
Adriano Oliveira <adrianooliveira@MacBook-Air-de-Adriano.local>
2019-05-26 18:12:52 -0300

    Adding exercice 1 and exercise 3.

---
+++ 1_java_basics/2_structure_of_a_java_class/exercise_1.java
@@ -0,0 +1,26 @@
+package a.b.c;
+
+import java.util.*;
+
+class D {
+  public static void main(String[] args) {
+    ArrayList<String> existing = new ArrayList<String>();
+
+    for (String arg : args) {
+      if (new E().exists(arg))
+        existing.add(arg);
+      }
+    }
+ }
+
+import java.io.*;
+
+class E {
+  public boolean exists(String name) {
+    File f = new File(name);
+    return f.exists();
+  }
+}
+
+// Compilation error: The import is out of order,
+// should be declared after package declaration.
