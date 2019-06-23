bf09761acab25148f590436398ce347b265cdb13
Ewerson dos Reis Carmo <ewersoncarmo@gmail.com>
2019-06-23 15:19:14 -0300

    Adding exercises 1 and 2

---
+++ 1_java_basics/3_executable_applications/exercise_1.java
@@ -0,0 +1,2 @@
+public static void main(String... args)
+// valid signature to run a java program
---
+++ 1_java_basics/3_executable_applications/exercise_2/A.java
@@ -0,0 +1,8 @@
+package exercise_2;
+  class A {
+    public static void main(String[] args) {
+    System.out.println("running");
+  }
+}
+
+// javac exercise_2/A.java e java exercise_2.A
