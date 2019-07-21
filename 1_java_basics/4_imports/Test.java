ac6d7f6004a8abaeced8dcf63033a168eee72bdc
Adriano Oliveira <adrianooliveira@MacBook-Air-de-Adriano.local>
2019-07-21 16:39:35 -0300

    Adding exercise 4 about imports.

---
+++ 1_java_basics/4_imports/Test.java
@@ -0,0 +1,9 @@
+import model.A;
+
+class Test {
+  public static void main(String[] args) {
+
+  new A("guilherme").print(); // Does not compile. The constructor
+  // of the class A is protected and it isn't visible.
+  }
+}
