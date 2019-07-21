94e7fe1f5074278c3a0a7b1e123f3ab58e1d8952
Ewerson dos Reis Carmo <ewersoncarmo@gmail.com>
2019-07-21 17:24:41 -0300

    Creating exercise 5

---
+++ 1_java_basics/4_imports/a/A.java
@@ -0,0 +1,4 @@
+package a;
+  class A {
+    b.B variable; // Compilation error. It is necessary to inform the full qualified name of the class B, like a.b.B
+}
---
+++ 1_java_basics/4_imports/a/b/B.java
@@ -0,0 +1,3 @@
+package a.b;
+  class B { // Class B has to be visible adding public modifier
+}
